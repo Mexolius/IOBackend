1e25419c9dc9a25307af29731af6ec240e78fe4c
PKopel <pawel.kopel2@gmail.com>
2021-03-26 16:47:30 +0100

    Add dependencies on jackson and KMongo, add code checking connection

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
@@ -6,6 +8,10 @@ plugins {
     application
     kotlin("jvm") version "1.4.32"
 }
+
+val compileKotlin: KotlinCompile by tasks
+
+compileKotlin.kotlinOptions.jvmTarget = "1.8"
 
 group = "com.gumi.moodle"
 version = "0.0.1"
@@ -20,6 +26,8 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("io.ktor:ktor-jackson:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("org.litote.kmongo:kmongo-coroutine:4.2.5")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
 }
--- src/main/kotlin/com/gumi/moodle/Application.kt
+++ src/main/kotlin/com/gumi/moodle/Application.kt
@@ -1,13 +1,16 @@
 package com.gumi.moodle
 
-import io.ktor.routing.*
-import io.ktor.http.*
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.jackson.*
 import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.routing.*
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
+
 
 fun main(args: Array<String>): Unit =
-        io.ktor.server.netty.EngineMain.main(args)
+    io.ktor.server.netty.EngineMain.main(args)
 
 /**
  * Please note that you can use any other name instead of *module*.
@@ -16,10 +19,21 @@ fun main(args: Array<String>): Unit =
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    install(ContentNegotiation) {
+        jackson()
+    }
+
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            val client = KMongo.createClient("mongodb://localhost:27017").coroutine
+            val users = client.getDatabase("test")
+                .getCollection<Jedi>()
+                .find()
+                .toList()
+            call.respondText("users: $users")
         }
     }
 }
 
+data class Jedi(val name: String, val age: Int)
