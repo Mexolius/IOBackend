1bfc22913af4b8116891d6d1f72783163a34d4f5
PKopel <pawel.kopel2@gmail.com>
2021-03-26 23:25:48 +0100

    GMS-41 Połączenie projektu z bazą - aktualizacja testów

--- src/test/kotlin/com/gumi/moodle/ApplicationTest.kt
+++ src/test/kotlin/com/gumi/moodle/ApplicationTest.kt
@@ -14,7 +14,7 @@ class ApplicationTest {
         withTestApplication({ module(testing = true) }) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("Hello World!", response.content)
+                assertEquals("users: []", response.content)
             }
         }
     }
