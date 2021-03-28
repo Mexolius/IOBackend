0baa82ef2df1dec4f32d86e2b3e7ede51ad25069
Pawel Miziołek <pawel00100@gmail.com>
2021-03-28 20:54:56 +0200

    GMS-47 Logowanie i rejestracja backend z endpointami Added basic auth Added logging check endpoint and registration

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("io.ktor:ktor-jackson:$ktor_version")
+    implementation("io.ktor:ktor-auth:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("org.litote.kmongo:kmongo-coroutine:4.2.5")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
