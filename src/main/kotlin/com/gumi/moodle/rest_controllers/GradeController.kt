957645840557d0c6ce4434939eedfe3049cff5f9
ptyszko <62699530+ptyszko@users.noreply.github.com>
2021-05-17 14:13:17 +0200

    Endpoint do oceniania wielu uczniów z 1 oceny (#32)
    * task budujący fat-jar jako dystrybucję + zmieniony test w ApplicationTest
    
    * Create test.yml
    
    * Create gradle.yml
    
    * remove test workflow
    
    * endpoint do wpisania konkretnej oceny wielu studentom
    
    * zmienione URL z /many na /grade/many
    
    * edit README.md
    
    * zmiana zachowania - zamiast usuwać niewymienionych studentów pomija ich

--- src/main/kotlin/com/gumi/moodle/rest_controllers/GradeController.kt
+++ src/main/kotlin/com/gumi/moodle/rest_controllers/GradeController.kt
@@ -84,6 +84,21 @@ fun Application.gradeRoutes() {
                         }
                     }
                 }
+                route("/grade/many/{$course_id}/{$grade_id}"){
+                    post {
+                        parameters(course_id, grade_id) { (courseID, gradeID) ->
+                            val grades = call.receive<Map<String, Int>>()
+                            val updated = dao.updateOne(
+                                courseID,
+                                combine(grades.map {
+                                        (k, v) -> Course::grades.posOp / Grade::studentPoints atKey k setTo v
+                                })
+                            ) { Course::_id eq it withGradeID gradeID }
+                            if (updated) call.respond(HttpStatusCode.OK)
+                            else call.respond(HttpStatusCode.NotModified)
+                        }
+                    }
+                }
                 route("/grade/{$course_id}/{$grade_id}/{$user_id}") {
                     post {
                         parameters(course_id, grade_id, user_id) { (courseID, gradeID, studentID) ->
